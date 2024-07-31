5f9c6d2f6dd44322982f86fb1f43ace4f696539b
vgguerra <victoevandro94@gmail.com>
2024-07-31 20:02:51 -0300

    MAVEN

---
+++ ContaBanco/ContaTerminal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ContaTerminal ct = new ContaTerminal();
+        int numeroConta = sc.nextInt();
+        ct.sc.
+    }
+}
